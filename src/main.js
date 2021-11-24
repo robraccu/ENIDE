01b3c76a733202085b07af6057be633ffe4993f0
Roberto Raccuglia <roberto.raccuglia@eng.it>
2021-11-24 15:36:52 +0100

    fix fine riga (#9)

--- src/main.js
+++ src/main.js
@@ -67,12 +67,12 @@ keycloak
           } else {
             console.warn(
               "Token not refreshed, valid for " +
-                Math.round(
-                  keycloak.tokenParsed.exp +
-                    keycloak.timeSkew -
-                    new Date().getTime() / 1000
-                ) +
-                " seconds"
+              Math.round(
+                keycloak.tokenParsed.exp +
+                keycloak.timeSkew -
+                new Date().getTime() / 1000
+              ) +
+              " seconds"
             );
           }
         })
